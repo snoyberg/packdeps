f56695741185077eed3758eed14e774b6e56684e
Michael Snoyman <michael@snoyman.com>
2017-04-03 09:04:58 +0000

    Use guessApproot

--- Foundation.hs
+++ Foundation.hs
@@ -63,7 +63,7 @@ type Form x = Html -> MForm Handler (FormResult x, Widget)
 -- Please see the documentation for the Yesod typeclass. There are a number
 -- of settings which can be configured by overriding methods here.
 instance Yesod App where
-    approot = ApprootMaster $ appRoot . settings
+    approot = guessApproot
 
     -- Disable sessions
     makeSessionBackend _ = return Nothing
