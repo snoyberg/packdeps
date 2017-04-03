5e04a34a1245dd38bf3956b1af895c39a744fc3f
Michael Snoyman <michael@snoyman.com>
2017-04-03 16:37:29 +0300

    Line buffering

--- app/main.hs
+++ app/main.hs
@@ -3,6 +3,10 @@ import Yesod.Default.Config (fromArgs)
 import Yesod.Default.Main   (defaultMain)
 import Settings             (parseExtra)
 import Application          (makeApplication)
+import System.IO            (hSetBuffering, stdout, stderr, BufferMode (LineBuffering))
 
 main :: IO ()
-main = defaultMain (fromArgs parseExtra) makeApplication
+main = do
+  hSetBuffering stdout LineBuffering
+  hSetBuffering stderr LineBuffering
+  defaultMain (fromArgs parseExtra) makeApplication
