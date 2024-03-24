056195200fb218db59d4ebc19169543ceddf9ec4
Andreas Abel <andreas.abel@ifi.lmu.de>
2024-03-24 18:59:59 +0100

    packdeps-cli: drop building with Cabal < 3.4

--- packdeps-cli/src/Distribution/PackDeps.hs
+++ packdeps-cli/src/Distribution/PackDeps.hs
@@ -46,7 +46,6 @@ import Distribution.PackageDescription
 import Distribution.PackageDescription.Parsec
 import Distribution.Parsec (Parsec, lexemeParsec, runParsecParser, simpleParsec)
 import Distribution.Parsec.FieldLineStream (fieldLineStreamFromBS)
-import Distribution.Types.CondTree
 import Distribution.Version
 import Distribution.Utils.ShortText
 import qualified Distribution.Utils.ShortText as ShortText
@@ -253,11 +252,7 @@ getLibDeps gpd = maybe [] condTreeConstraints' (condLibrary gpd) ++ customDeps
     -- we only interested in Cabal default upper bound
     -- See cabal-install Distribution.Client.ProjectPlanning defaultSetupDeps
     defSetupDeps = [Dependency (mkPackageName "Cabal") (earlierVersion $ mkVersion [1,25])
-#if MIN_VERSION_Cabal(3, 4, 0)
                                mainLibSet
-#else
-                               mempty
-#endif
                    ]
 
 condTreeConstraints' :: Monoid c => CondTree ConfVar c a  -> c
@@ -271,11 +266,7 @@ condTreeConstraints' = go where
 notFlagCondition :: Condition ConfVar -> Bool
 notFlagCondition (Var (OS _))     = True
 notFlagCondition (Var (Arch _))   = True
-#if MIN_VERSION_Cabal(3, 4, 0)
 notFlagCondition (Var (PackageFlag _)) = False
-#else
-notFlagCondition (Var (Flag _))   = False
-#endif
 notFlagCondition (Var (Impl _ _)) = True
 notFlagCondition (Lit _)          = True
 notFlagCondition (CNot a)         = notFlagCondition a
